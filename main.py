e117d10b14f37777c7bd275125e9d70955abf72d
Cesarddds <131389209+Cesarddds@users.noreply.github.com>
2023-06-16 23:13:35 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import generador
+
+lista_numeros = generador.generar_numeros()
+print("Lista generada:")
+generador.mostrar_lista(lista_numeros)
+
+print("Lista ordenada:")
+generador.ordenar_lista(lista_numeros)
